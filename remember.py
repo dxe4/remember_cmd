49ae80115e7d1acbea72c8b30a045d8ee15cd0cc
charalambosp <papaloizouc@gmail.com>
2014-01-12 21:44:03 +0000

    fixed a minor bug and added some code to test what function will be called depending on input

--- remember.py
+++ remember.py
@@ -8,6 +8,54 @@ from collections import OrderedDict
 
 # TODO consider service running at startup not sure if it worths doing yet
 
+def append_to_store(command, key=None, meta_data=None):
+    """
+        Appends a command in the store.
+        You can retrieve the command back in 3 different ways:
+            1) Search the actual command
+                e.g. command is history | grep foo, you can search all commands for foo
+            2) Save a key for the command
+                e.g. command is ps -aux you can add a key 1, then you can retrieve it by giving one
+            3) Save metadata
+                e.g. command is pip install -r requirements.txt
+                and metadata is "use pip and requirements.txt to install dependencies"
+                Then you can search by "requirements.txt" or by "dependencies"
+
+    @param command: The command to save
+    @param key: When key is used it will be saved in a key value store
+    @param meta_data: When metadata is used you will be able to search by metadata too
+    """
+    pass
+
+
+def delete_from_store(command=None, key=None, meta_data=None, regex=False):
+    """
+        Delete command(s) from store.
+        Multiple values can be deleted if regex mode is used,
+            or if the exactly the same meta_data exists in more than one commands
+    @param command: If command is given the command(s) will be deleted.
+    @param key: If key is given the command corresponding to that key will be deleted
+    @param meta_data: If meta_data is given the command(s) corresponding to meta_data will be deleted
+    @param regex: If regex is set to true the command and meta_data will be used as regex
+            and multiple commands may be deleted at once
+    """
+    pass
+
+
+def find_in_store(command=None, key=None, meta_data=None, regex=False):
+    """
+        Find commands from store.
+        Multiple values can be found if regex mode is used,
+            or if the exactly the same meta_data exists in more than one commands
+    @param command: If command is given will try to find the corresponding command(s)
+    @param key: If key is given the command matching the key will be returned
+    @param meta_data: If metadata is given the corresponding command(s) will be returned
+    @param regex: If regex mode is given meta_data and command will be used as regex
+    @return The found command(s)
+    """
+    pass
+
+
 class DB:
 
     def __init__(self):
@@ -165,54 +213,6 @@ class ArgHandler:
                 for arg, value in self.args.items() if value}
 
 
-def append_to_store(command, key=None, meta_data=None):
-    """
-        Appends a command in the store.
-        You can retrieve the command back in 3 different ways:
-            1) Search the actual command
-                e.g. command is history | grep foo, you can search all commands for foo
-            2) Save a key for the command
-                e.g. command is ps -aux you can add a key 1, then you can retrieve it by giving one
-            3) Save metadata
-                e.g. command is pip install -r requirements.txt
-                and metadata is "use pip and requirements.txt to install dependencies"
-                Then you can search by "requirements.txt" or by "dependencies"
-
-    @param command: The command to save
-    @param key: When key is used it will be saved in a key value store
-    @param meta_data: When metadata is used you will be able to search by metadata too
-    """
-    pass
-
-
-def delete_from_store(command=None, key=None, meta_data=None, regex=False):
-    """
-        Delete command(s) from store.
-        Multiple values can be deleted if regex mode is used,
-            or if the exactly the same meta_data exists in more than one commands
-    @param command: If command is given the command(s) will be deleted.
-    @param key: If key is given the command corresponding to that key will be deleted
-    @param meta_data: If meta_data is given the command(s) corresponding to meta_data will be deleted
-    @param regex: If regex is set to true the command and meta_data will be used as regex
-            and multiple commands may be deleted at once
-    """
-    pass
-
-
-def find_in_store(command=None, key=None, meta_data=None, regex=False):
-    """
-        Find commands from store.
-        Multiple values can be found if regex mode is used,
-            or if the exactly the same meta_data exists in more than one commands
-    @param command: If command is given will try to find the corresponding command(s)
-    @param key: If key is given the command matching the key will be returned
-    @param meta_data: If metadata is given the corresponding command(s) will be returned
-    @param regex: If regex mode is given meta_data and command will be used as regex
-    @return The found command(s)
-    """
-    pass
-
-
 def exec_command(command: str):
     # TODO later use for --exec parameter
     # exec_command("python foo.py")
@@ -225,10 +225,16 @@ def exec_command(command: str):
 if __name__ == "__main__":
     arg_handler = ArgHandler()
     _args = arg_handler.get_input()
-    if not arg_handler.state_validation():
+    if arg_handler.state_validation():
         raise NotImplementedError("Need to give a proper warning which args have dependencies")
     if not _args:
         arg_handler.parser.print_help()
+    # {'list': (True, 'other_args')}
+    for k, v in _args.items():
+        cmd, search, add, delete =\
+            map(lambda arg: (arg is v[1], arg), ["command_args", "search_args", "add_args", "delete_args"])
+        print(cmd, search, add, delete)
+        
     db = DB()
 #store.commands["hisgrep"] = "history | grep "
 # save_store(store)
